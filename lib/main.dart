3f0a757a86d43b07836f4f6dbba69b1f311b0ffe
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-15 19:01:46 +0300

    add StartApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(QuizApp());
+void main() => runApp(const QuizApp());
 
 class QuizApp extends StatelessWidget {
-
   const QuizApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Image.asset("assets/images/quiz-logo.png"),
-          ],
+        body: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Colors.deepPurple,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/quiz-logo.png",
+                width: 200,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "Learn Flutter the fun way!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                ),
+                child: const Text(
+                  "Start Quiz",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
